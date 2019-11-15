5b4ccea0f3a70b9457933bc7aff7c35646887930
Arpit Trivedi <arpittrivedi2425@gmail.com>
2019-11-15 14:28:10 +0530

    hardcoded testing

--- routes/dialogflowroutes.js
+++ routes/dialogflowroutes.js
@@ -1,7 +1,11 @@
 const dialogflow=require('dialogflow');
 const config=require('../config/keys');
-const sessionClient=new dialogflow.SessionsClient();
-const sessionPath=sessionClient.sessionPath(config.googleProjectID,config.dialogFlowSessionID)
+const uuid = require('uuid');
+const sessionClient=new dialogflow.SessionsClient({
+  keyFilename:"C:/Users/Arpit/Downloads/reactappagent-cactkb-3903e1a48a93.json"}
+);
+const sessionId = uuid.v4();
+const sessionPath=sessionClient.sessionPath(config.googleProjectID,sessionId)
 module.exports=app=>{
     app.get('/',(req,res)=>{
         res.send({'Hi':'there'})
@@ -13,15 +17,16 @@ module.exports=app=>{
                 queryInput: {
                   text: {
                     // The query to send to the dialogflow agent
-                    text: req.body.text,
+                    text:"I want to learn java",
                     // The language used by the client (en-US)
-                    languageCode: 'en-US',
+                    languageCode: config.dialogFlowSessionLanguageCode,
                   },
                 },
               };
-              let responses=await sessionClient.detectIntent(request);
+              const responses = await sessionClient.detectIntent(request);
+              
 
-            res.send(responses[0].queryResult)
+            res.send(responses[0].queryResult.fulfillmentText)
             });
             
         app.post('/api/df_event_query',(req,res)=>{
@@ -30,3 +35,48 @@ module.exports=app=>{
         
         
 }
+// const dialogflow = require('dialogflow');
+// const uuid = require('uuid');
+
+// /**
+//  * Send a query to the dialogflow agent, and return the query result.
+//  * @param {string} projectId The project to be used
+//  */
+// async function runSample(projectId = 'reactappagent-cactkb') {
+//   // A unique identifier for the given session
+//   const sessionId = uuid.v4();
+
+//   // Create a new session
+//   const sessionClient = new dialogflow.SessionsClient({
+    
+//     keyFilename:"C:/Users/Arpit/Downloads/reactappagent-cactkb-3903e1a48a93.json"
+
+//   });
+//   const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+//   // The text query request.
+//   const request = {
+//     session: sessionPath,
+//     queryInput: {
+//       text: {
+//         // The query to send to the dialogflow agent
+//         text: 'hi',
+//         // The language used by the client (en-US)
+//         languageCode: 'en-US',
+//       },
+//     },
+//   };
+
+//   // Send request and log result
+//   const responses = await sessionClient.detectIntent(request);
+//   console.log('Detected intent');
+//   const result = responses[0].queryResult;
+//   console.log(`  Query: ${result.queryText}`);
+//   console.log(`  Response: ${result.fulfillmentText}`);
+//   if (result.intent) {
+//     console.log(`  Intent: ${result.intent.displayName}`);
+//   } else {
+//     console.log(`  No intent matched.`);
+//   }
+// }
+// runSample();
