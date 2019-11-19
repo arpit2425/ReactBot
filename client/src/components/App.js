29e18b4fad79901facbfb12753945931ae92046a
Arpit Trivedi <arpittrivedi2425@gmail.com>
2019-11-19 23:01:00 +0530

    Created chatbot component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Landing from './pages/Landing';
 import Header from './Header';
+import Chatbot from './chatbot/chatbot'
 import {BrowserRouter, Route} from 'react-router-dom'
 import About from './pages/About';
 import Shops from './shop/Shops';
@@ -15,6 +16,7 @@ const App=()=>{
                 <Route exact path="/" component={Landing}/>
                 <Route exact path="/Shop" component={Shops}/>
                 <Route exact path="/About" component={About}/>
+                <Chatbot/>
             </div>
             </BrowserRouter>
         </div>
---
+++ client/src/components/chatbot/chatbot.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const chatbot=()=>{
+    return(
+        <div>
+            <h1>This is chatbot
+            </h1>
+            </div>
+    )
+}
+export default chatbot;
