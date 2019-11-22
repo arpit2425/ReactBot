88858344cee1c5afa72025067297f98d56f39724
Arpit Trivedi <arpittrivedi2425@gmail.com>
2019-11-22 22:39:05 +0530

    universal cookies

--- client/src/components/chatbot/chatbot.js
+++ client/src/components/chatbot/chatbot.js
@@ -1,59 +1,111 @@
-import React,{Component} from 'react';
-import axios from 'axios/index';
-class chatbot extends Component{
-    constructor(props)
-    {
+import React, { Component } from 'react';
+import axios from "axios/index";
+
+import Cookies from 'universal-cookie';
+import { v4 as uuid } from 'uuid';
+
+import Message from './Message';
+
+const cookies = new Cookies();
+
+class Chatbot extends Component {
+    messagesEnd;
+    talkInput;
+
+    constructor(props) {
         super(props);
-        this.state={
-            messages:[],
+        // This binding is necessary to make `this` work in the callback
+        this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
+        this.state = {
+            messages: []
         };
+        if (cookies.get('userID') === undefined) {
+            cookies.set('userID', uuid(), { path: '/' });
+        }
     }
-    async df_text_query(text)
-    {
-        let says={
-            speaks:"me",
-            msg:{
-                text:{
-                    text:text
+
+    async df_text_query (queryText) {
+        let says = {
+            speaks: 'user',
+            msg: {
+                text : {
+                    text: queryText
                 }
+            }
+        }
+        this.setState({ messages: [...this.state.messages, says]});
+        const res = await axios.post('/api/df_text_query',  {text: queryText, userID: cookies.get('userID')});
 
+        for (let msg of res.data.fulfillmentMessages) {
+            says = {
+                speaks: 'bot',
+                msg: msg
             }
-        };
-        this.setState({messages:[...this.messages.state,says]});
-        const res=await axios.post('/api/df_text_query',{text});
-        for(let msg of res.data.fulfillmentMessages)
-        {
-            says={
-                speaks:'bot',
-                msg:msg
+            this.setState({ messages: [...this.state.messages, says]});
+        }
+    };
+
+
+    async df_event_query(eventName) {
+
+        const res = await axios.post('/api/df_event_query',  {event: eventName, userID: cookies.get('userID')});
+
+        for (let msg of res.data.fulfillmentMessages) {
+            let says = {
+                speaks: 'bot',
+                msg: msg
             }
-            this.setState({messages:[...this.messages.state,says]});
+
+            this.setState({ messages: [...this.state.messages, says]});
+        }
+    };
+
+    componentDidMount() {
+        this.df_event_query('Welcome');
+    }
+
+    componentDidUpdate() {
+        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+        this.talkInput.focus();
+    }
+
+    renderMessages(stateMessages) {
+        if (stateMessages) {
+            return stateMessages.map((message, i) => {
+                if (message.msg && message.msg.text && message.msg.text.text) {
+                        return <Message key={i} speaks={message.speaks} text={message.msg.text.text}/>;
+                    } else {
+                        return <h2>Cards</h2>;
+                    }
+
+                }
+            )
+        } else {
+            return null;
         }
     }
-    async df_event_query(event)
-    {
-        const res=await axios.post('/api/df_event_query',{event});
-        for(let msg of res.data.fulfillmentMessages)
-        {
-           let says={
-                speaks:'bot',
-                msg:msg
-            }
-            this.setState({messages:[...this.messages.state,says]});
-        }
 
+    _handleInputKeyPress(e) {
+        if (e.key === 'Enter') {
+            this.df_text_query(e.target.value);
+            e.target.value = '';
+        }
     }
 
-    render()
-    {
-    return(
-        <div style={{height:400,width:400,float:"right"  }}>
-            <div id="chatbot" style={{width:"100%",height:"100%",overflow:"auto"}}>
-                <h2>Chatbot</h2>
-                <input type="text"/>
+    render() {
+        return (
+            <div style={{height: 400, width: 400, float: 'right'}}>
+                <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
+                    <h2>Chatbot</h2>
+                    {this.renderMessages(this.state.messages)}
+                    <div ref={(el) => { this.messagesEnd = el; }}
+                         style={{ float:"left", clear: "both" }}>
+                    </div>
+                    <input type="text" ref={(input) => { this.talkInput = input; }}  onKeyPress={this._handleInputKeyPress}  />
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
-}
-export default chatbot;
+
+export default Chatbot;
